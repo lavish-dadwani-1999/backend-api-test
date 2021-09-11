9cb3850752f0b05ae065f25a3359745a5149a6cc
Lavish Dadwani <lavishdadwanii@gmail.com>
2021-09-11 21:12:28 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var user_routes = require('./routes/user');
 var bug_routes = require("./routes/bug")
+var dotenv = require("dotenv")
 var normal_routes = require("./routes/normalUser")
+dotenv.config()
 var app = express();
 require('./db.js');
 
-
+console.log(process.env.SOMES_ENV);
 
 app.use(express.json());
 
